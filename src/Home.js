b3be0befb571999e17f815fdc25748efec8f8e5e
George Manuel <georgemanuel1997@gmail.com>
2020-07-19 21:32:37 -0400

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pricing from './Pricing';
+
+
+function Home() {
+  return (
+      <div>
+        <Pricing />
+    </div>
+  );
+}
+
+export default Home;
